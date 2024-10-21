630bfec119a1353a09ef172daac86413c853faac
reala <reala@AZOLA>
2024-10-21 18:29:19 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using ASPNETCore_DB.Data;
+using ASPNETCore_DB.Interfaces;
+using ASPNETCore_DB.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPNETCore_DB.Controllers
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SQLiteDBContext _context;
+        private readonly IDBInitializer _seedDatabase;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, SQLiteDBContext context, IDBInitializer seedDatabase,UserManager<IdentityUser>userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _seedDatabase = seedDatabase;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
+            ViewData["UserName"] = _userManager.GetUserName(this.User);
+            
+
+            // Check user roles
+            if (this.User.IsInRole("Admin"))
+            {
+                ViewData["UserRole"] = "Admin";
+            }
+            else if (this.User.IsInRole("User"))
+            {
+                ViewData["UserRole"] = "User";
+            }
+
+            return View();
+        }
+
+        // Other action methods...
+        public IActionResult SeedDatabase()
+        {
+           // _seedDatabase.Initialize(_context);
+            ViewBag.SeedDbFeedback = "Database created and Student Table populated with Data. Check Database folder.";
+            return View("SeedDatabase");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+    
+
+    
---
+++ Data/SQLiteDBContext.cs
@@ -0,0 +1,22 @@
+﻿using ASPNETCore_DB.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCore_DB.Data
+{
+    public class SQLiteDBContext : /*Identity*/DbContext
+    {
+        public SQLiteDBContext(DbContextOptions<SQLiteDBContext> options) : base(options)
+        {        
+                
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+
+        }
+    }
+}
